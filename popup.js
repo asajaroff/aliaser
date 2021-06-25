ef61019fe551a4f530d89673182801cabab89d70
asajaroff <asajaroff@gmail.com>
2021-06-24 23:10:49 -0300

    UI thoughts... WIP

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+let addAlias = document.getElementById("addAlias");
 
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
